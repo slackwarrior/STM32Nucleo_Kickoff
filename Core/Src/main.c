4b4dc770e130655dba512c2850918c72338ea2d9
Karol Zmyslowski <karol.zmyslowski@gmail.com>
2023-01-09 02:01:44 +0100

    main.c: Insert test semihosting debug

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -48,6 +48,8 @@
 static const uint32_t PERIOD = 1000;
 static uint32_t prev_ticks = 0;
 static uint32_t new_ticks;
+
+static char buff_to_debug[15];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -104,6 +106,22 @@ int main(void)
       new_ticks = HAL_GetTick() / PERIOD;
       if (new_ticks > prev_ticks) {
           prev_ticks = new_ticks;
+          ITM_SendChar('V');
+          ITM_SendChar('a');
+          ITM_SendChar('l');
+          ITM_SendChar(':');
+       
+           
+          // snprintf(buff_to_debug, "%d", prev_ticks); 
+          // ITM_SendChar(buff_to_debug[0]);
+          // ITM_SendChar(buff_to_debug[1]);
+          // ITM_SendChar(buff_to_debug[2]);
+          // ITM_SendChar(buff_to_debug[3]);
+          // ITM_SendChar(buff_to_debug[4]);
+          // ITM_SendChar(buff_to_debug[5]);
+          // ITM_SendChar(buff_to_debug[6]);
+          ITM_SendChar('\r');
+
       }
     /* USER CODE END WHILE */
 
