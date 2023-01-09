67cc0ac432e0ed1feb2053cda1b2419747b39b80
Karol Zmyslowski <karol.zmyslowski@gmail.com>
2023-01-09 01:11:47 +0100

    Added User LED LD2 controlled by User Button B1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -102,8 +102,13 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-      HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-      HAL_Delay(333);
+      if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin)==GPIO_PIN_RESET){
+          HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+      }
+      else {
+          HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+      }
   }
   /* USER CODE END 3 */
 }
